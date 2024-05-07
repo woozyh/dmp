632756653d6449af60cc7436beba536ec55c713e
woozy_fedora <bradbtccbt@gmail.com>
2024-05-07 22:07:03 +0330

    added new feature (dataToCsv()) and some clustering algorithms

--- src/cosineSimilarity.py
+++ src/cosineSimilarity.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3.11.8
-from numpy.linalg import norm
-from numpy        import dot, array
-from itertools    import combinations
+from numpy.linalg    import norm
+from numpy           import dot, array
+from itertools       import combinations
+from sklearn.cluster import KMeans # 
+from sklearn.cluster import AgglomerativeClustering  #-Hierarchical clustering method
+
 import sqlite3
 
 class CosineSimilarity(object):
 
     def __init__(self) -> None:
-        self.databse = sqlite3.connect("termDoc.db")
-        self.cursor  = self.databse.cursor()
-        self.tables  = self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
+        self.databse   = sqlite3.connect("termDoc.db")
+        self.cursor    = self.databse.cursor()
+        self.tables    = self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
+        self.cosineSim = [[0.0 for i in range(20)] for i in range(20)]
+        self.cosineDis = [[0.0 for i in range(20)] for i in range(20)]
 
     def documentCombinations(self) -> list:
         docComb = list(combinations([f[0] for f in self.tables], 2))        
@@ -25,13 +30,37 @@ class CosineSimilarity(object):
         for comb in self.documentCombinations():
             vector = self.vectorization(comb[0], comb[1])
             cosineSimilarity = dot(vector[0], vector[1])/((norm(vector[0]))*(norm(vector[1])))
-            print(f"cosine similarity for {comb}: {cosineSimilarity}")
-        # self.databse.close()
-        # self.cursor.close()
+            self.cosineSim[int(comb[0][3:])-1][int(comb[1][3:])-1] = cosineSimilarity
+            self.cosineSim[int(comb[1][3:])-1][int(comb[0][3:])-1] = cosineSimilarity
+            self.cosineDis[int(comb[0][3:])-1][int(comb[1][3:])-1] = 1 - cosineSimilarity
+            self.cosineDis[int(comb[1][3:])-1][int(comb[0][3:])-1] = 1 - cosineSimilarity
 
+    def kMeans(self):
+        self.kmeans = KMeans(n_clusters=19).fit(self.cosineDis)
+        self.dataToCsv(self.kmeans.cluster_centers_, "kMeansDis")
+
+    def agglomerativeClustering(self):
+        self.agglomerative = AgglomerativeClustering(n_clusters=19).fit(self.cosineSim)
+
+    def draw(self):
+        pass
+
+    def dataToCsv(self, data, name):
+        with open(name+'.csv', 'w') as file:
+            file.write("docs, ")
+            for id in range(1, 21):
+                file.write(f"doc{id}, \t")
+            for _ in data:
+                file.write("\ndoc, \t")
+                for __ in _:
+                    file.write(f"{__}, \t")
+            file.close()
 def main():
     ins = CosineSimilarity()
     ins.cosineSimilarity()
+    ins.kMeans()
+    ins.dataToCsv(ins.cosineDis, "cosineDis")
+    ins.dataToCsv(ins.cosineSim, "cosineSim")
 
 if __name__ == "__main__":
     main()
