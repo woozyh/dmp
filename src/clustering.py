e08fb174dc3015784bf4388fdc73f30895b2b9c7
woozy_fedora <bradbtccbt@gmail.com>
2024-05-15 10:12:49 +0330

    building clustering classes.

---
+++ src/clustering.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.11.8
+
+from sklearn.cluster import kmeans, AgglomerativeClustering
+import pandas as pd
+
+class partitioning(object):
+
+    def __init__(self) -> None:
+        pass
+
+    def kmeans(self):
+        pass
+
+class hierarichal(object):
+    
+    def __init__(self) -> None:
+        pass
+    
+    def ahc(self):
+        pass
+    
